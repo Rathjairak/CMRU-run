49195bad1ab62533a678b1306852f2163bbdf645
Rathjairak <rathjairak@gmail.com>
2016-06-29 13:35:00 +0700

    Setup CMRU Finish

--- app/src/main/java/cmru/jairak/rath/cmrurun/MapsActivity.java
+++ app/src/main/java/cmru/jairak/rath/cmrurun/MapsActivity.java
@@ -13,16 +13,17 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private double cmruLatADouble =18.807089, cmruLngADouble = 98.986621;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.my_layout);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
+    } //Main Method
 
 
     /**
@@ -38,9 +39,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+        // Setup to CMRU
+        LatLng latLng = new LatLng(cmruLatADouble, cmruLngADouble);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+        // ตัวเลข 16 คือใกล้โลกเท่าไหร่
+    } //Onmapready
+
+
+
+}// Main Class
